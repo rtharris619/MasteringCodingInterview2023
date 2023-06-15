3685f0c267b57fc8be450bce51881fbef138f090
Ryan Harris <ryan@synthesis.co.za>
2023-06-15 17:40:54 +0200

    Arrays

--- Section6/arrays.js
+++ Section6/arrays.js
@@ -1,4 +1,15 @@
 function driver() {
+    const newArray = new MyArray();
+    newArray.push('hey');
+    newArray.push('you');
+
+    newArray.push('!');
+    newArray.delete(1);
+
+    console.log(newArray);
+}
+
+function basics() {
     let strings = ['a', 'b', 'c', 'd'];
     
     strings.push('e'); // O(1)
@@ -10,6 +21,43 @@ function driver() {
     strings.splice(2, 0, 'alien'); // O(n)
 
     console.log(strings);
+}
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
 module.exports = {driver};
